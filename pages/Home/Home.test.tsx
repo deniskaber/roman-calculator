813acb7dd2bf9ed3d9a21f19b835350f28fd1ef9
Denis Berezin <mr.denis.berezin@gmail.com>
2022-08-23 19:53:50 +0300

    Add input validation test

--- pages/Home/Home.test.tsx
+++ pages/Home/Home.test.tsx
@@ -6,40 +6,51 @@ import * as converterMock from '../../lib/utils/arabic-to-roman-numeral-convert'
 jest.mock('../../lib/utils/arabic-to-roman-numeral-convert');
 
 describe('Home', () => {
-  it('renders a number input', () => {
+  let input: HTMLInputElement;
+  let output: HTMLInputElement;
+
+  const user = userEvent.setup();
+  const latinToRomanNumeralConvertSpy = jest
+    .spyOn(converterMock, 'arabicToRomanNumeralConvert')
+    .mockImplementation((input: number) => {
+      return `${input} converted`;
+    });
+
+  beforeEach(() => {
     render(<Home />);
 
-    const input = screen.getByLabelText('Input a number here:');
+    input = screen.getByLabelText('Input a number here:');
+    output = screen.getByLabelText('Converted Roman Numeral:');
+  });
 
+  it('renders a number input', () => {
     expect(input).toBeInTheDocument();
   });
 
   it('renders an output field', () => {
-    render(<Home />);
-
-    const input = screen.getByLabelText('Converted Roman Numeral:');
-
     expect(input).toBeInTheDocument();
   });
 
-  it('displays converted number in output field on user input', async () => {
+  it('uses arabicToRomanNumeralConvert to make number conversion', async () => {
     const user = userEvent.setup();
-    const latinToRomanNumeralConvertSpy = jest
-      .spyOn(converterMock, 'arabicToRomanNumeralConvert')
-      .mockImplementation((input: number) => {
-        return `${input} converted`;
-      });
-
-    render(<Home />);
-
-    const input = screen.getByLabelText('Input a number here:');
 
     await user.click(input);
     await user.keyboard('123');
 
-    const output = screen.getByLabelText('Converted Roman Numeral:');
-
     expect(latinToRomanNumeralConvertSpy).toHaveBeenCalledWith(123);
+  });
+
+  it('displays converted number in output field on user input', async () => {
+    await user.click(input);
+    await user.keyboard('123');
+
     expect((output as HTMLInputElement).value).toBe('123 converted');
+  });
+
+  it('validates user input to be a number between 1 and 1000 inclusive', async () => {
+    await user.click(input);
+    await user.keyboard('0');
+
+    expect((output as HTMLInputElement).value).toBe('');
   });
 });
