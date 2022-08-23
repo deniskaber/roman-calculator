ff2b19124717a0d84f72b9c12b529442308d17fa
Denis Berezin <mr.denis.berezin@gmail.com>
2022-08-23 17:44:16 +0300

    Add number input implementation

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,18 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [numberText, setNumberText] = useState('');
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Roman Numerals Calculator App</title>
+        <title>Latin to Roman Numerals Calculator App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        Content
+        <label>
+          Input a latin number here:
+          <input
+            className={styles['number-input']}
+            value={numberText}
+            onChange={(e) => setNumberText(e.target.value)}
+          />
+        </label>
       </main>
     </div>
-  )
+  );
 }
